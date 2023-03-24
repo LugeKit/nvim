772711e2568fedee79179dd2ff851318d82c5427
lijiaqi.k1 <lijiaqi.k1@bytedance.com>
2023-03-24 10:57:47 +0800

    feat: add movements shortcuts for telescope
    Change-Id: I2b51a8013b5e334ee28456962dd67d0c361f9328

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -1,3 +1,14 @@
 map('n', '<leader>ff', ':Telescope find_files<CR>')
 map('n', '<leader>fd', ':Telescope tags<CR>')
 map('n', '<leader>fs', ':Telescope live_grep<CR>')
+
+require('telescope').setup {
+	defaults = {
+		mappings = {
+			i = {
+				['<C-j>'] = require('telescope.actions').move_selection_next,
+				['<C-k>'] = require('telescope.actions').move_selection_previous,
+			},
+		},
+	},
+}
