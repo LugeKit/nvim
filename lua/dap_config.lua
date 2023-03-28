ab52e2d7a635edf428cdf89a5d1730a738f7e9a7
LugeKit <263635591@qq.com>
2023-03-29 00:11:49 +0800

    feat: add shortcuts for dap

--- lua/dap_config.lua
+++ lua/dap_config.lua
@@ -1,12 +1,40 @@
 local keymap = {
+	d = {
+		name = "Debug",
+		c = { "<cmd>DapContinue<CR>", "Continue" },
+		n = { "<cmd>DapStepOver<CR>", "Step Over" },
+		i = { "<cmd>DapStepInto<CR>", "Step Into" },
+		o = { "<cmd>DapStepOut<CR>", "Step Out" },
+		b = { "<cmd>DapToggleBreakpoint<CR>", "Toggle Breakpoint" },
+		B = { "<cmd>lua require('dap').list_breakpoints()<CR>", "List Breakpoints" },
+		C = { "<cmd>lua require('dap').clear_breakpoints()<CR>", "Clear Breakpoints" },
+		q = { "<cmd>DapToggleRepl<CR>", "Toggle Repl" },
+		x = { "<cmd>DapTerminate<CR>", "Terminate" },
+		r = { "<cmd>lua require('dap').run_to_cursor()<CR>", "Run To Cursor" },
+	},
+}
+
+local keymap_v = {
 	name = "Debug",
-	["<F5>"] = { "<cmd>DapContinue<CR>", "Continue" },
-	["<F6>"] = { "<cmd>DapStepOver<CR>", "Step Over" },
-	["<F7>"] = { "<cmd>DapStepInto<CR>", "Step Into" },
-	["<F8>"] = { "<cmd>DapStepOut<CR>", "Step Out" },
-	b = { "<cmd>DapToggleBreakPoint<CR>", "Toggle Breakpoint" },
-	r = { "<cmd>DapToggleRepl<CR>", "Toggle Repl" },
+	e = { "<cmd>lua require('dapui').eval()<CR>", "Evaluate" },
 }
 
 local wk = require("which-key")
-wk.register({ d = keymap }, { prefix = "<leader>" })
+wk.register(keymap, { prefix = "<leader>" })
+wk.register(keymap_v, { mode = "v", prefix = "<leader>" })
+
+require("dap-go").setup({})
+
+local dapui = require("dapui")
+dapui.setup {}
+
+local dap = require("dap")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
