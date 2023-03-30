72a1a818c6a77787c3068428f26443e043f1576a
LugeKit <263635591@qq.com>
2023-03-30 20:07:34 +0800

    feat: add shortcuts for Telescope Dynamic Workspace Symbols
    Change-Id: I41c3f71f63d7eaf7c8b4c1bab0f783b953a3f0b0

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -14,6 +14,7 @@ wk.register({
 	f = {
 		name = "Telescope",
 		f = { "<cmd>Telescope find_files<CR>", "Find File" },
+		d = { "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", "Workspace Dynamic Symbols"},
 		s = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
 		a = { "<cmd>Telescope git_status<CR>", "Git Status" },
 	},
