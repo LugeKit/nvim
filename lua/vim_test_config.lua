378e297afcee1f90daeecafa0c448e279d96d2ce
LugeKit <263635591@qq.com>
2023-03-31 12:49:46 +0800

    feat: clean nvim-tree shortcuts
    Change-Id: I3065a0d6fe42ef0216b4e10a26bf36cdbc0449f1

--- lua/vim_test_config.lua
+++ lua/vim_test_config.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_create_user_command('GoTestFile', 'TestFile -gcflags=all=-l', {})
-vim.api.nvim_create_user_command('GoTestNearest', 'TestNearest -gcflags=all=-l', {})
-vim.api.nvim_create_user_command('GoTestSuite', 'TestSuite -gcflags=all=-l', {})
+vim.api.nvim_create_user_command('GoTestFile', 'TestFile -gcflags=all=-l <args>', {})
+vim.api.nvim_create_user_command('GoTestNearest', 'TestNearest -gcflags=all=-l <args>', {})
+vim.api.nvim_create_user_command('GoTestSuite', 'TestSuite -gcflags=all=-l <args>', {})
