6dfab877a54e4a42e8c08ba5f01561f8ca9cf3e0
LugeKit <263635591@qq.com>
2023-04-01 23:16:28 +0800

    feat: autoselect one when trigger completion

--- lua/cmp_config.lua
+++ lua/cmp_config.lua
@@ -1,54 +1,54 @@
 local cmp = require("cmp")
 
-cmp.setup {
+cmp.setup({
 	snippet = {
 		expand = function(args)
 			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
-	preselect = cmp.PreselectMode.None,
+	preselect = cmp.PreselectMode.Item,
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select, count = 1 }),
-		['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select, count = 1 }),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm { select = false },
-	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select, count = 1 }),
+		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select, count = 1 }),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lua' },
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' },
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
 	}, {
-		{ name = 'buffer' },
-	})
-}
-
-cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' },
-	}, {
-		{ name = 'buffer' },
-	})
+		{ name = "buffer" },
+	}),
 })
 
-cmp.setup.cmdline({ '', '?' }, {
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" },
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+cmp.setup.cmdline({ "", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = 'buffer' },
+		{ name = "buffer" },
 	},
 })
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = 'path' },
+		{ name = "path" },
 	}, {
-		{ name = 'cmdline' },
-	})
+		{ name = "cmdline" },
+	}),
 })
