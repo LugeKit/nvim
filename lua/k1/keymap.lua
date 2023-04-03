20f803c600d958d4d22de4c5e413565e5d8c1e19
LugeKit <263635591@qq.com>
2023-04-03 12:47:28 +0800

    fix: <C-k> useless in visual mode
    Change-Id: I1fa777d13fd8ad2fecaf6839f147ea70c7c2517f

--- lua/k1/keymap.lua
+++ lua/k1/keymap.lua
@@ -1,18 +1,18 @@
 local wk = require("which-key")
 wk.register({
-	["1"] = { "1gt", "Tab 1" },
-	["2"] = { "2gt", "Tab 2" },
-	["3"] = { "3gt", "Tab 3" },
-	["4"] = { "4gt", "Tab 4" },
-	["5"] = { "5gt", "Tab 5" },
-	["6"] = { "6gt", "Tab 6" },
-	["7"] = { "7gt", "Tab 7" },
-	["<TAB>"] = { "gt", "Next Tab" },
-	["<S-TAB>"] = { "gT", "Previous Tab" },
-	t = { "<cmd>tabnew<CR>", "New Tab" },
-	["<BS>"] = { "<cmd>bd<CR>", "Delete Current Buffer" },
+    ["1"] = { "1gt", "Tab 1" },
+    ["2"] = { "2gt", "Tab 2" },
+    ["3"] = { "3gt", "Tab 3" },
+    ["4"] = { "4gt", "Tab 4" },
+    ["5"] = { "5gt", "Tab 5" },
+    ["6"] = { "6gt", "Tab 6" },
+    ["7"] = { "7gt", "Tab 7" },
+    ["<TAB>"] = { "gt", "Next Tab" },
+    ["<S-TAB>"] = { "gT", "Previous Tab" },
+    t = { "<cmd>tabnew<CR>", "New Tab" },
+    ["<BS>"] = { "<cmd>bd<CR>", "Delete Current Buffer" },
 }, {
-	prefix = "<leader>",
+    prefix = "<leader>",
 })
 
 local utils = require("k1.utils")
@@ -23,10 +23,10 @@ local opts = utils.opts
 map("", "<ESC>", "<ESC><cmd>silent noh<CR>", opts({}))
 
 -- movements
-map("n", "<C-j>", "10j", opts({}))
-map("v", "<C-j>", "10j", opts({}))
-map("n", "<C-k>", "10k", opts({}))
-map("v", "<C-k>", "10k", opts({}))
+map("n", "<C-j>", "10<down>", opts({}))
+map("v", "<C-j>", "10<down>", opts({}))
+map("n", "<C-k>", "10<up>", opts({}))
+map("v", "<C-k>", "10<up>", opts({}))
 map("n", "<C-h>", "^", opts({}))
 map("v", "<C-h>", "^", opts({}))
 map("n", "<C-l>", "$", opts({}))
@@ -51,6 +51,3 @@ map("v", "y", "y`>0", opts({ desc = "Jump To End After Yank" }))
 
 map("n", "<F1>", "<cmd>tabnew<CR><cmd>terminal<CR>a", opts({ desc = "Open Terminal In New Tab" }))
 map("t", "<ESC>", "<C-\\><C-n>", opts({}))
-
--- training: disable some keys
-map("n", "c$", "<nop>", opts({}))
