5acc71d3c5b4afac754eb470395c5310d0760593
LugeKit <263635591@qq.com>
2023-04-05 14:06:37 +0800

    feat: use tab to jump between snippets, use <C-j>, <C-k> to select

--- lua/cmp_config.lua
+++ lua/cmp_config.lua
@@ -21,29 +21,36 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<Tab>"] = cmp.mapping(function(fallback)
+		["<C-j>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-			-- they way you will only jump inside the snippet region
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		end, { "i", "s", "c" }),
+		["<C-k>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+			else
+				fallback()
+			end
+		end, { "i", "s", "c" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-		["<C-Space>"] = cmp.mapping.complete(),
+		end),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping({
 			i = function(fallback)
