0a7d2061222794b9ec83d87d8b13bc167fdaf619
LugeKit <263635591@qq.com>
2023-04-06 13:35:45 +0800

    chore: add comments in start.lua
    Change-Id: I352aff5563689467be8f29b6ba581ee60df0556e

--- start.lua
+++ start.lua
@@ -1,80 +1,103 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	"neovim/nvim-lspconfig",
-	{ "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
+  "neovim/nvim-lspconfig",
 
-	{
-		"glepnir/lspsaga.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		commit = "04617d1",
-	},
+  -- syntax highlight
+  { "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
 
-	"williamboman/mason.nvim",
-	"mfussenegger/nvim-dap",
-	"leoluz/nvim-dap-go",
-	"rcarriga/nvim-dap-ui",
-	"vim-test/vim-test",
+  -- lsp enhancement
+  {
+    "glepnir/lspsaga.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    commit = "04617d1",
+  },
+  -- lsp server managment
+  "williamboman/mason.nvim",
 
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/cmp-nvim-lua",
-	"hrsh7th/nvim-cmp",
-	"L3MON4D3/LuaSnip",
-	"ray-x/lsp_signature.nvim",
-	"folke/neodev.nvim",
+  -- debugger
+  "mfussenegger/nvim-dap",
+  "leoluz/nvim-dap-go",
+  "rcarriga/nvim-dap-ui",
 
-	"weilbith/nvim-lsp-smag", -- automated tags
+  -- test helper, one command to run test
+  "vim-test/vim-test",
 
-	"folke/tokyonight.nvim",
-	"nvim-tree/nvim-tree.lua",
-	"nvim-tree/nvim-web-devicons",
-	"airblade/vim-gitgutter",
-	"tpope/vim-fugitive",
+  -- all about auto completion
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-nvim-lua",
+  "hrsh7th/nvim-cmp",
+  "ray-x/lsp_signature.nvim",
+  "folke/neodev.nvim",
 
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- snippets for expand completion and more
+  "L3MON4D3/LuaSnip",
 
-	"nvim-lualine/lualine.nvim", -- status line
-	"RRethy/vim-illuminate", -- highlight cursor word
-	{
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 600
-			require("which-key").setup({})
-		end,
-	},
-	"nvim-lualine/lualine.nvim",
-	"jose-elias-alvarez/null-ls.nvim",
+  -- automated tags
+  "weilbith/nvim-lsp-smag",
 
-	"windwp/nvim-autopairs",
+  "folke/tokyonight.nvim",
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons",
+
+  -- git enhancement
+  "airblade/vim-gitgutter",
+  "tpope/vim-fugitive",
+
+  -- finder
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- status line
+  "nvim-lualine/lualine.nvim",
+
+  -- highlight cursor word
+  "RRethy/vim-illuminate",
+
+  -- show shortcuts in floating view
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 600
+      require("which-key").setup({})
+    end,
+  },
+  -- lsp linter, dignostics, formatter and more
+  "jose-elias-alvarez/null-ls.nvim",
+
+  -- pair helper, auto complete pairs
+  "windwp/nvim-autopairs",
 })
 
 -- user configs
 require("mason").setup({})
 require("neodev").setup({
-	library = { plugins = { "nvim-dap-ui" }, types = true },
+  library = { plugins = { "nvim-dap-ui" }, types = true },
 }) -- before lspconfig
 require("k1").setup()
 require("lualine_config")
