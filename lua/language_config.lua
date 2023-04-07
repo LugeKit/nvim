4247fc86a21cc16ae2c23e57b46488aa2415ab1f
LugeKit <263635591@qq.com>
2023-04-07 20:40:14 +0800

    feat: add support for python

--- lua/language_config.lua
+++ lua/language_config.lua
@@ -1,37 +1,40 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 lspconfig.gopls.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 -- lspconfig.golangci_lint_ls.setup{
 -- 	capabilities = capabilities,
 -- }
 lspconfig.lua_ls.setup({
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 })
 lspconfig.vimls.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 lspconfig.bashls.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
 })
 lspconfig.clangd.setup({
-	capabilities = capabilities,
+  capabilities = capabilities,
+})
+lspconfig.pyright.setup({
+  capabilities = capabilities,
 })
