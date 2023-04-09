12ad4cd6abbd8938b36161065e06ec9e7a961119
LugeKit <263635591@qq.com>
2023-04-09 17:33:09 +0800

    feat: <F1> to call floaterm in insert mode

--- lua/floaterm_config.lua
+++ lua/floaterm_config.lua
@@ -3,6 +3,7 @@ local util = require("k1.util")
 local keymap = {
   { "n", "<F1>", "<cmd>FloatermToggle<CR>", {} },
   { "t", "<F1>", "<cmd>FloatermToggle<CR>", {} },
+  { "i", "<F1>", "<cmd>FloatermToggle<CR>", {} },
 }
 
 util.map_with_dicts(keymap)
