8e5856c677625324c9d8a0e4325d328580ccd562
LugeKit <263635591@qq.com>
2023-04-09 16:46:13 +0800

    feat: change formatting autocmd

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -21,11 +21,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- 2. It can go inside std lib (while telescope.builtin.lsp_definitions cannot)
     -- TODO@k1 in the future, try to implement a telescope extension as lsp_finder (including references, definitions, etc.)
     local keymap = {
-      { "n", "<leader>rn", vim.lsp.buf.rename,                             opts({ desc = "Rename" }) },
-      { "n", "<leader>ca", vim.lsp.buf.code_action,                        opts({ desc = "Code Action" }) },
-      { "n", "<leader>fd", "<cmd>lua vim.lsp.buf.definition()<CR>zz",      opts({ desc = "Go To Definition" }) },
-      { "n", "<leader>fD", "<cmd>lua vim.lsp.buf.type_definition()<CR>zz", opts({ desc = "Go To Type Definition" }) }
+      { "n", "<leader>rn", vim.lsp.buf.rename,                        opts({ desc = "Rename" }) },
+      { "n", "<leader>ca", vim.lsp.buf.code_action,                   opts({ desc = "Code Action" }) },
+      { "n", "<leader>fd", "<cmd>lua vim.lsp.buf.definition()<CR>zz", opts({ desc = "Go To Definition" }) },
+      {
+        "n",
+        "<leader>fD",
+        "<cmd>lua vim.lsp.buf.type_definition()<CR>zz",
+        opts({ desc = "Go To Type Definition" }),
+      },
     }
     util.map_with_dicts(keymap)
+  end,
+})
+
+local augroup = vim.api.nvim_create_augroup("LspFormat", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function(args)
+    vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 1000 })
   end,
 })
