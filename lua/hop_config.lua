c8be511f9ffa3dcf1bde6c6318a1bbfbbd193e70
LugeKit <263635591@qq.com>
2023-04-11 23:59:33 +0800

    feat: hop keymap settings and update dependencies

--- lua/hop_config.lua
+++ lua/hop_config.lua
@@ -5,18 +5,17 @@ local directions = require("hop.hint").HintDirection
 hop.setup({})
 
 local keymap = {
-  { "n", "f", function() hop.hint_char1({ direction = nil, current_line_only = true }) end, util.opts() },
-  { "o", "f", function() hop.hint_char1({ direction = nil, current_line_only = true }) end, util.opts() },
-  { "n", "t", function() hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = -1 }) end,
+  { "", "f", function() hop.hint_char1({ direction = nil, current_line_only = true }) end, util.opts() },
+  { "", "t", function() hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = -1 }) end,
     util.opts() },
-  { "o", "t", function() hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = -1 }) end,
-    util.opts() },
-  { "n", "<leader>j", function() hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR }) end, util.opts() },
-  { "o", "<leader>j", function() hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR }) end, util.opts() },
-  { "n", "<leader>k", function() hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR }) end,
-    util.opts() },
-  { "o", "<leader>k", function() hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR }) end,
-    util.opts() },
+  { "", "<leader>j", function() hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR }) end,
+    util.opts({ desc = "Hop-j" }) },
+  { "", "<leader>k", function() hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR }) end,
+    util.opts({ desc = "Hop-k" }) },
 }
 
 util.map_with_dicts(keymap)
+util.unmap("s", "f")
+util.unmap("s", "t")
+util.unmap("s", "<leader>j")
+util.unmap("s", "<leader>k")
