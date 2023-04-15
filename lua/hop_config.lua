f9dfe6d436077802bc7dc7d70f01a8bb0046d7de
LugeKit <263635591@qq.com>
2023-04-16 00:55:26 +0800

    feat: add <leader><leader>f to trigger globally hop

--- lua/hop_config.lua
+++ lua/hop_config.lua
@@ -5,19 +5,53 @@ local directions = require("hop.hint").HintDirection
 hop.setup({})
 
 local keymap = {
-  { { "n", "x", "o" }, "f", function() hop.hint_char1({ direction = nil, current_line_only = true }) end, util.opts() },
-  { { "n", "x", "o" }, "t",
-    function() hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = -1 }) end,
+  {
+    { "n", "x", "o" },
+    "f",
+    function()
+      hop.hint_char1({ direction = nil, current_line_only = true })
+    end,
+    util.opts()
+  },
+  {
+    { "n", "x", "o" },
+    "<leader><leader>f",
+    function()
+      hop.hint_char1({ direction = nil, current_line_only = false })
+    end,
+    util.opts()
+  },
+  {
+    { "n", "x", "o" },
+    "t",
+    function()
+      hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = -1 })
+    end,
     util.opts() },
-  { { "n", "x", "o" }, "T",
-    function() hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = 1 }) end,
-    util.opts() },
-  { { "n", "x", "o" }, "<leader>j",
-    function() hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR }) end,
-    util.opts({ desc = "Hop-j" }) },
-  { { "n", "x", "o" }, "<leader>k",
-    function() hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR }) end,
-    util.opts({ desc = "Hop-k" }) },
+  {
+    { "n", "x", "o" },
+    "T",
+    function()
+      hop.hint_char1({ direction = nil, current_line_only = true, hint_offset = 1 })
+    end,
+    util.opts()
+  },
+  {
+    { "n", "x", "o" },
+    "<leader>j",
+    function()
+      hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR })
+    end,
+    util.opts({ desc = "Hop-j" })
+  },
+  {
+    { "n", "x", "o" },
+    "<leader>k",
+    function()
+      hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR })
+    end,
+    util.opts({ desc = "Hop-k" })
+  },
 }
 
 util.map_with_dicts(keymap)
