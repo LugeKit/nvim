e9137d5e1e62908f0a99eb385f2ae417f07855d7
LugeKit <263635591@qq.com>
2023-04-21 18:07:49 +0800

    feat: add support for operator motions
    Change-Id: I8314ad08fde200d3ca70a1c3c6306732a8c24327

--- lua/treesitter_config.lua
+++ lua/treesitter_config.lua
@@ -8,6 +8,18 @@ treesitter.setup({
     additional_vim_regex_highlighting = false,
   },
   textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ip"] = "@parameter.inner",
+        ["ap"] = "@parameter.outer",
+      }
+    },
     move = {
       enable = true,
       set_jumps = true,
@@ -15,7 +27,22 @@ treesitter.setup({
         ["]f"] = { query = "@function.outer", desc = "Next function start" },
         ["]c"] = { query = "@class.outer", desc = "Next class start" },
         ["]p"] = { query = "@parameter.inner", desc = "Next parameter start" },
-      }
+      },
+      goto_next_end = {
+        ["]F"] = { query = "@function.outer", desc = "Next function end" },
+        ["]C"] = { query = "@class.outer", desc = "Next class end" },
+        ["]P"] = { query = "@parameter.inner", desc = "Next parameter end" },
+      },
+      goto_previous_start = {
+        ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+        ["[c"] = { query = "@class.outer", desc = "Previous class start" },
+        ["[p"] = { query = "@parameter.inner", desc = "Previous parameter start" },
+      },
+      goto_previous_end = {
+        ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+        ["[C"] = { query = "@class.outer", desc = "Previous class end" },
+        ["[P"] = { query = "@parameter.inner", desc = "Previous parameter end" },
+      },
     },
   }
 })
