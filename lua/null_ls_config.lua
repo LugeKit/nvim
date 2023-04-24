d1eab1ba711de94a60e687d0e205cdb700ab96e9
LugeKit <263635591@qq.com>
2023-04-25 00:25:02 +0800

    feat: add tidy for html diagnostics

--- lua/null_ls_config.lua
+++ lua/null_ls_config.lua
@@ -5,5 +5,6 @@ null_ls.setup({
     null_ls.builtins.formatting.goimports.with({
       extra_args = { "-local", vim.trim(vim.fn.system({ "go", "list", "-m" })) },
     }),
+    null_ls.builtins.diagnostics.tidy,
   },
 })
