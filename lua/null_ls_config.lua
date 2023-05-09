8a7f8939075e8fe01a8f640ecc2f72b19c649337
LugeKit <263635591@qq.com>
2023-05-09 14:40:29 +0800

    chore: update dependencies
    Change-Id: Ib7f4503acdd345511b906e267c008570c3835109

--- lua/null_ls_config.lua
+++ lua/null_ls_config.lua
@@ -1,13 +1,13 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.goimports.with({
-      extra_args = { "-local", vim.trim(vim.fn.system({ "go", "list", "-m" })) },
-    }),
-    null_ls.builtins.diagnostics.tidy,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.gofumpt,
-  },
+	sources = {
+		null_ls.builtins.formatting.goimports.with({
+			extra_args = { "-local", vim.trim(vim.fn.system({ "go", "list", "-m" })) },
+		}),
+		null_ls.builtins.diagnostics.tidy,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gofumpt,
+	},
 })
