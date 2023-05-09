cfeaecc0e4790fd26ae294ed5cbbac7c04ce4cef
LugeKit <263635591@qq.com>
2023-05-09 17:44:05 +0800

    refactor: change format augroup to null-ls
    Change-Id: Ib710c9e815ca5e5d9b2d91a558423ba06aea64b1

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -58,19 +58,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			},
 		}
 		util.map_with_dicts(keymap)
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormat", { clear = true }),
-			callback = function(args)
-				vim.lsp.buf.format({
-					bufnr = args.buf,
-					timeout_ms = 1000,
-					filter = function(c)
-						return c.name == "null-ls"
-					end,
-				})
-			end,
-		})
 	end,
 })
 
--- lua/null_ls_config.lua
+++ lua/null_ls_config.lua
@@ -1,13 +1,32 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
+	log_level = "trace",
 	sources = {
 		null_ls.builtins.formatting.goimports.with({
 			extra_args = { "-local", vim.trim(vim.fn.system({ "go", "list", "-m" })) },
 		}),
 		null_ls.builtins.diagnostics.tidy,
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.stylua.with({}),
+		null_ls.builtins.formatting.gofumpt.with({}),
 	},
+	on_attach = function(c, bufnr)
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		if c.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function(client)
+							return client.name == "null-ls"
+						end,
+					})
+				end,
+			})
+		end
+	end,
 })
